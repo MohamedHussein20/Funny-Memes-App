e8ba2c0134741cbb76bde1f65c566cd7ddc4e939
Mohamed Hussein <mohussein1279@gmail.com>
2025-02-06 21:15:40 +0200

    Main Screen Http Request is finished

--- app/src/main/java/com/memo/funnymemesapp/MainActivity.kt
+++ app/src/main/java/com/memo/funnymemesapp/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.memo.funnymemesapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.memo.funnymemesapp.models.Meme
 import com.memo.funnymemesapp.screens.DetailScreen
 import com.memo.funnymemesapp.screens.MainScreen
+import com.memo.funnymemesapp.utils.RetrofitInstance
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +29,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
+            var memesList by remember {
+                mutableStateOf(listOf<Meme>())
+            }
+            val scope = rememberCoroutineScope()
+            LaunchedEffect(key1 = true) {
+                scope.launch(Dispatchers.IO) {
+                    val response = try {
+                        RetrofitInstance.api.getMemesList()
+                    } catch (e: HttpException) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "App Error: ${e.message()}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        return@launch
+                    } catch (e: IOException) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "IO Error: ${e.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        return@launch
+                    }
+                    if (response.isSuccessful && response.body() != null) {
+                        with(Dispatchers.Main) {
+                            memesList = response.body()!!.data.memes
+                        }
+                    }
+                }
+            }
             NavHost(
                 navController = navController,
                 startDestination = "Main Screen"
             ) {
                 composable("Main Screen") {
-                    MainScreen()
+                    MainScreen(memesList = memesList , navController = navController)
                 }
                 composable("Detail Screen") {
                     DetailScreen()
--- app/src/main/java/com/memo/funnymemesapp/screens/MainScreen.kt
+++ app/src/main/java/com/memo/funnymemesapp/screens/MainScreen.kt
@@ -1,8 +1,10 @@
 package com.memo.funnymemesapp.screens
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.memo.funnymemesapp.models.Meme
 
 @Composable
-fun MainScreen(){
+fun MainScreen( memesList: List<Meme>, navController: NavController){
 
 }
